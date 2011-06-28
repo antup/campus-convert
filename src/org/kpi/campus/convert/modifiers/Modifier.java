3d6864396832705b417ef25c9a4ce33088f32497
Igor <kiv14@mail.ru>
2011-06-29 00:28:08 +0300

    Test commit

---
+++ src/org/kpi/campus/convert/modifiers/Modifier.java
@@ -0,0 +1,13 @@
+/**
+ * Package for modifiers.
+ */
+package org.kpi.campus.convert.modifiers;
+
+/**
+ * Modifier interface
+ * @author Igor Kokhanevych
+ *
+ */
+public interface Modifier {
+	Object modify(Object obj);
+}
